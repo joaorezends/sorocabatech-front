9019ab6bb014cde27dd7006209418463cdfe8f79
joaorezends <joaocarlosrezendejunior@gmail.com>
2025-01-05 17:12:48 -0300

    add react form

--- src/routes/loja/entrar.lazy.tsx
+++ src/routes/loja/entrar.lazy.tsx
@@ -1,19 +1,58 @@
 import logo from '/logo.svg';
+import { useForm } from '@tanstack/react-form';
 import { createLazyFileRoute, Link } from '@tanstack/react-router'
-import { useCallback } from 'react';
 
 const SignIn = () => {
-  useCallback(() => {
-    console.log();
-  }, [])
+  const { handleSubmit, Field } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value)
+    },
+  })
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-tertiary">
       <div className="flex flex-col max-w-md py-16 px-20 bg-white shadow">
         <img className="mx-auto rounded-full w-24 h-24" src={logo} alt="sorocaba.tech logo" width="96" height="96" />
-        <form className="flex flex-col mt-7">
-          <input className='input mb-3' name="email" type="email" placeholder="Insira aqui seu e-mail" />
-          <input className="input mb-3" name="password" type="text" placeholder="Senha" />
+        <form
+          className="flex flex-col mt-7"
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            handleSubmit();
+          }}
+        >
+          <Field
+            name="email"
+            children={(field) => (
+              <input
+                className="input mb-3"
+                type="email"
+                name={field.name}
+                value={field.state.value}
+                placeholder="Insira aqui seu e-mail"
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
+            )}
+          />
+          <Field
+            name="password"
+            children={(field) => (
+              <input
+                className="input mb-3"
+                type="password"
+                name={field.name}
+                value={field.state.value}
+                placeholder="Senha"
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
+            )}
+          />
           <Link className="block mb-6 text-secondary text-xs" to="/loja/recuperar-senha">
             Esqueci minha senha
           </Link>
