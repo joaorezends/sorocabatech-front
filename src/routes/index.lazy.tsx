7c2160de9d91271f679c01fc7294d21399e1323b
joaorezends <joaocarlosrezendejunior@gmail.com>
2025-01-06 08:47:14 -0300

    add react query

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,32 +1,27 @@
 import logo from '/logo.svg';
+import { useQuery } from '@tanstack/react-query';
 import { createLazyFileRoute } from '@tanstack/react-router'
-import { useEffect, useState } from 'react'
 import { Link, Social, SocialType } from '../types'
 import Icon from '../components/Icon'
 
 const Index = () => {
-  const [links, setLinks] = useState<Link[]>([])
-  const [socials, setSocials] = useState<Social[]>([])
-
-  useEffect(() => {
-    const load = async () => {
+  const { data: links } = useQuery<Link[]>({
+    queryKey: ['links'],
+    queryFn: async () => {
       const response = await fetch(import.meta.env.VITE_API_URL + '/links')
       const links = await response.json()
-      setLinks(links)
+      return links;
     }
+  })
 
-    load()
-  }, [])
-
-  useEffect(() => {
-    const load = async () => {
+  const { data: socials } = useQuery<Social[]>({
+    queryKey: ['socials'],
+    queryFn: async () => {
       const response = await fetch(import.meta.env.VITE_API_URL + '/socials')
       const socials = await response.json()
-      setSocials(socials)
+      return socials;
     }
-
-    load()
-  }, [])
+  })
 
   return (
     <div className="min-h-screen bg-tertiary">
@@ -34,7 +29,7 @@ const Index = () => {
         <img className="mx-auto rounded-full w-24 h-24" src={logo} alt="sorocaba.tech logo" width="96" height="96" />
         <h1 className="mt-4 text-center text-lg font-bold text-primary">@sorocaba.tech</h1>
         <ul className="flex justify-center gap-5 mt-5">
-          {socials.map((social, index) =>
+          {socials?.map((social, index) =>
             <li key={index}>
               {social.type === SocialType.WHATSAPP && (
                 <a className="text-3xl text-primary" href={'https://api.whatsapp.com/send?phone=' + social.value} target="_blank" rel="noopener">
@@ -50,7 +45,7 @@ const Index = () => {
           )}
         </ul>
         <ul className="flex flex-col gap-4 mt-9">
-          {links.map((link, index) =>
+          {links?.map((link, index) =>
             <li key={index}>
               <a className="button button-quaternary button-lg" href={link.url} target="_blank" rel="noopener">
                 {link.name}
