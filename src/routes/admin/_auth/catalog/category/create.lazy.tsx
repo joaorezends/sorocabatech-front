f4bb03d4cfbdf1375e07dd2c9628da1badbbd5ec
joaorezends <joaocarlosrezendejunior@gmail.com>
2025-01-17 00:19:23 -0300

    remove react-form of create category form

--- src/routes/admin/_auth/catalog/category/create.lazy.tsx
+++ src/routes/admin/_auth/catalog/category/create.lazy.tsx
@@ -8,62 +8,59 @@ import {
 } from "@/components/ui/breadcrumb"
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
-import { useForm } from '@tanstack/react-form'
 import { createLazyFileRoute, Link } from '@tanstack/react-router'
-import { Category } from '../../../../../types'
-import { useMutation } from '@tanstack/react-query'
 import { z } from 'zod'
-import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormLabel } from "@/components/ui/form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { useCallback } from "react"
 
 export const Route = createLazyFileRoute('/admin/_auth/catalog/category/create')({
   component: RouteComponent,
 })
 
+const formSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().max(4000).optional(),
+  seoTitle: z.string().max(70).optional(),
+  seoDescription: z.string().max(250).optional(),
+})
+
 function RouteComponent() {
-  const { mutate, isPending } = useMutation({
-    mutationFn: async (data: Partial<Category>) => {
-      (Object.keys(data) as (keyof Category)[]).forEach((key) => {
-        if (data[key] === '') {
-          delete data[key]
-        }
-      })
+  const submitHandler = useCallback(async (data: z.infer<typeof formSchema>) => {
+    (Object.keys(data) as (keyof typeof data)[]).forEach((key) => {
+      if (data[key] === '') {
+        delete data[key]
+      }
+    })
 
-      const response = await fetch(
-        import.meta.env.VITE_API_URL + '/catalog/categories',
-        {
-          method: 'POST',
-          credentials: 'include',
-          headers: {
-            'content-type': 'application/json',
-          },
-          body: JSON.stringify(data),
+    const response = await fetch(
+      import.meta.env.VITE_API_URL + '/catalog/categories',
+      {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'content-type': 'application/json',
         },
-      )
-      
-      console.log(response)
-    },
-  })
+        body: JSON.stringify(data),
+      },
+    )
+    
+    console.log(response)
+  }, [])
 
-  const { handleSubmit, Field, Subscribe } = useForm({
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       name: '',
       description: '',
       seoTitle: '',
       seoDescription: '',
-    },
-    validators: {
-      onBlur: z.object({
-        name: z.string(),
-        description: z.string().max(4000),
-        seoTitle: z.string().max(70),
-        seoDescription: z.string().max(250),
-      }),
-    },
-    onSubmit: async ({ value }) => mutate(value),
+    }
   })
 
   return (
@@ -88,123 +85,109 @@ function RouteComponent() {
             </Breadcrumb>
           </div>
         </header>
-        <form
-          className="flex flex-1 flex-col gap-4 p-4 pt-0"
-          onSubmit={(e) => {
-            e.preventDefault()
-            e.stopPropagation()
-            handleSubmit()
-          }}
-        >
-          <Card>
-            <CardHeader>
-              <CardTitle>Informações principais</CardTitle>
-            </CardHeader>
-            <CardContent className="grid gap-6">
-              <Field
-                name="name"
-                children={(field) => (
-                  <div className="grid gap-2">
-                    <Label htmlFor="name">Nome da categoria *</Label>
-                    <Input
-                      id="name"
-                      name={field.name}
-                      value={field.state.value}
-                      required
-                      placeholder="Ex. Carregadores"
-                      onBlur={field.handleBlur}
-                      onChange={(e) => field.handleChange(e.target.value)}
-                    />
-                  </div>
-                )}
-              />
-              <Field
-                name="description"
-                children={(field) => (
-                  <div className="grid gap-2">
-                    <div className="flex items-center">
-                      <Label htmlFor="description">Descrição</Label>
-                      <span className="ml-auto text-sm">{field.state.value.length} de 4000 caracteres</span>
+        <Form {...form}>
+          <form className="flex flex-1 flex-col gap-4 p-4 pt-0" onSubmit={form.handleSubmit(submitHandler)}>
+            <Card>
+              <CardHeader>
+                <CardTitle>Informações principais</CardTitle>
+              </CardHeader>
+              <CardContent className="grid gap-6">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <div className="grid gap-2">
+                      <FormLabel>Nome da categoria *</FormLabel>
+                      <FormControl>
+                        <Input
+                          id="name"
+                          required
+                          {...field}
+                        />
+                      </FormControl>
                     </div>
-                    <Textarea
-                      id="description"
-                      name={field.name}
-                      value={field.state.value}
-                      maxLength={4000}
-                      rows={3}
-                      onBlur={field.handleBlur}
-                      onChange={(e) => field.handleChange(e.target.value)}
-                    />
-                  </div>
-                )}
-              />
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>SEO</CardTitle>
-            </CardHeader>
-            <CardContent className="grid gap-6">
-              <Field
-                name="seoTitle"
-                children={(field) => (
-                  <div className="grid gap-2">
-                    <div className="flex items-center">
-                      <Label htmlFor="seoTitle">Tag Title</Label>
-                      <span className="ml-auto text-sm">{field.state.value.length} de 70 caracteres</span>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <div className="grid gap-2">
+                      <div className="flex items-center">
+                        <FormLabel>Descrição</FormLabel>
+                        <span className="ml-auto text-sm">{field.value?.length} de 4000 caracteres</span>
+                      </div>
+                      <FormControl>
+                        <Textarea
+                          id="description"
+                          maxLength={4000}
+                          rows={3}
+                          {...field}
+                        />
+                      </FormControl>
                     </div>
-                    <Input
-                      id="seoTitle"
-                      name={field.name}
-                      value={field.state.value}
-                      maxLength={70}
-                      onBlur={field.handleBlur}
-                      onChange={(e) => field.handleChange(e.target.value)}
-                    />
-                  </div>
-                )}
-              />
-              <Field
-                name="seoDescription"
-                children={(field) => (
-                  <div className="grid gap-2">
-                    <div className="flex items-center">
-                      <Label htmlFor="seoDescription">Meta Tag Description</Label>
-                      <span className="ml-auto text-sm">{field.state.value.length} de 250 caracteres</span>
+                  )}
+                />
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>SEO</CardTitle>
+              </CardHeader>
+              <CardContent className="grid gap-6">
+                <FormField
+                  control={form.control}
+                  name="seoTitle"
+                  render={({ field }) => (
+                    <div className="grid gap-2">
+                      <div className="flex items-center">
+                        <FormLabel>Tag Title</FormLabel>
+                        <span className="ml-auto text-sm">{field.value?.length} de 70 caracteres</span>
+                      </div>
+                      <FormControl>
+                        <Input
+                          id="seoTitle"
+                          maxLength={70}
+                          {...field}
+                        />
+                      </FormControl>
                     </div>
-                    <Textarea
-                      id="seoDescription"
-                      name={field.name}
-                      value={field.state.value}
-                      maxLength={250}
-                      rows={3}
-                      onBlur={field.handleBlur}
-                      onChange={(e) => field.handleChange(e.target.value)}
-                    />
-                  </div>
-                )}
-              />
-            </CardContent>
-          </Card>
-          <div className="flex justify-end gap-4">
-            <Button variant="secondary" asChild>
-              <Link to="/admin/catalog/category/list">
-                  Cancelar
-              </Link>
-            </Button>
-            <Subscribe
-              selector={(state) => [state.canSubmit, state.isSubmitting]}
-              children={([canSubmit, isSubmitting]) => (
-                <Button
-                  type="submit"
-                  disabled={!canSubmit || isPending}
-                >
-                  {isSubmitting || isPending ? '...' : 'Criar categoria'}
-                </Button>
-              )}
-            />
-          </div>
-        </form>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="seoDescription"
+                  render={({ field }) => (
+                    <div className="grid gap-2">
+                      <div className="flex items-center">
+                        <FormLabel>Meta Tag Description</FormLabel>
+                        <span className="ml-auto text-sm">{field.value?.length} de 250 caracteres</span>
+                      </div>
+                      <FormControl>
+                        <Textarea
+                          id="seoDescription"
+                          maxLength={250}
+                          rows={3}
+                          {...field}
+                        />
+                      </FormControl>
+                    </div>
+                  )}
+                />
+              </CardContent>
+            </Card>
+            <div className="flex justify-end gap-4">
+              <Button variant="secondary" asChild>
+                <Link to="/admin/catalog/category/list">
+                    Cancelar
+                </Link>
+              </Button>
+              <Button type="submit" disabled={!form.formState.isDirty || !form.formState.isValid}>
+                {form.formState.isDirty && form.formState.isValid ? 'Criar categoria' : '...'}
+              </Button>
+            </div>
+          </form>
+        </Form>
       </div>
     </div>
   )
